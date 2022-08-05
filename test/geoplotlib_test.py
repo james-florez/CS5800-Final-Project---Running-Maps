7e4a019bc22bea882ae17bf93f29fcd9c1be9b14
James <jamesflorez11@gmail.com>
2022-08-05 11:25:15 -0700

    Created geoplotlib_test.py and made a data directory with test CSV files.

---
+++ test/geoplotlib_test.py
@@ -0,0 +1,22 @@
+import numpy
+import pyglet
+import geoplotlib
+from geoplotlib.utils import read_csv
+import matplotlib
+
+dot = False
+if dot:
+    data = read_csv("../data/boston_test.csv")
+    geoplotlib.dot(data, point_size=3)
+else:
+    data = read_csv("../data/boston_test2.csv")
+    geoplotlib.graph(data,
+                     src_lat='src_lat',
+                     src_lon='src_lon',
+                     dest_lat='dest_lat',
+                     dest_lon='dest_lon',
+                     color='hot_r',
+                     alpha=100,
+                     linewidth=20)
+
+geoplotlib.show()
