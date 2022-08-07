c3d5b37a26d4a4d19e070acd2de153a945ced9fe
James <jamesflorez11@gmail.com>
2022-08-07 12:18:55 -0700

    Adding docstrings to Graph.py and Node.py

--- src/Graph.py
+++ src/Graph.py
@@ -2,14 +2,31 @@ from src.Node import Node
 
 
 class Graph:
+    """A class used to represent a Graph (city map/route).
+
+    Attributes:
+        adjacency_list ([[(node_index, distance)]]: adjacency list representation of the Graph with distance values
+        points_of_interest (set): names of points of interest in the Graph
+        nodes (list): Nodes in the Graph
+    """
 
     def __init__(self):
+        """Initializes a Node object."""
+
         # TODO possibly make the adjacencyList use dictionaries
         self.adjacency_list = []
         self.points_of_interest = set()
         self.nodes = []
 
     def add_node(self, node: Node) -> bool:
+        """Adds a Node to the Graph representing a street intersection.
+
+        Args:
+            node (Node): the Node to be added
+        Returns:
+            bool: True if the Node was added and False if a Node with the same index already exists in the Graph
+        """
+
         # Validate parameters
         if node.get_index() < len(self.adjacency_list):
             return False  # Node already exists
@@ -25,6 +42,16 @@ class Graph:
         return True
 
     def add_edge(self, start_index: int, end_index: int, distance: int) -> bool:
+        """Adds an edge to the Graph representing a street.
+
+        Args:
+            start_index (int): the index of the Node on the 1st side of the edge
+            end_index (int): the index of the Node on the 2nd side of the edge
+            distance (int): the length of the edge in feet
+        Returns:
+             bool: True if the edge was added and False if either Node index does not exist in the Graph
+        """
+
         # Validate parameters
         if start_index >= len(self.adjacency_list) or end_index >= len(self.adjacency_list):
             return False  # Node does not exist
@@ -42,18 +69,38 @@ class Graph:
         return True
 
     def get_num_points_of_interest(self) -> int:
+        """Gets the number of the points of interest in the Graph.
+
+        Returns:
+            int: number of points of interest
+        """
         return len(self.points_of_interest)
 
     def get_points_of_interest(self) -> set:
+        """Gets the names of the points of interest in the Graph.
+
+        Returns:
+            set: names of points of interest
+        """
         return self.points_of_interest
 
     def get_node(self, node_index: int) -> Node or None:
+        """Gets the Node with the given node index.
+
+        Returns:
+            Node or None: The Node if the node index is valid or None if it is invalid
+        """
         if node_index >= len(self.nodes):
             return None
         else:
             return self.nodes[node_index]
 
     def get_distance(self, start_node_index: int, end_node_index: int) -> int:
+        """Gets the distance (feet) of the edge between the Nodes given by the indices.
+
+        Returns:
+            int: the distance in feet between the Nodes or 0 if the Nodes are not connected
+        """
         edges = self.adjacency_list[start_node_index]
         for edge in self.adjacency_list[start_node_index]:
             if edge[0] == end_node_index:
--- src/Node.py
+++ src/Node.py
@@ -1,23 +1,63 @@
 class Node:
-    """This class represents a Node in a Graph."""
+    """A class used to represent a Node (street intersection) in a Graph (city map).
+
+    Attributes:
+        index (str): a number used to reference the Node
+        latitude (float): latitude of the street intersection
+        longitude (float): longitude of the street intersection
+    """
 
     def __init__(self, index: int, latitude: float, longitude: float, points_of_interest: [str]):
+        """Initializes a Node object.
+
+        Args:
+            index (str): a number used to reference the Node
+            latitude (float): latitude of the street intersection
+            longitude (float): longitude of the street intersection
+            points_of_interest ([str]): names of the points of interest visible from this street intersection
+        """
+
         self.index = index
         self.latitude = latitude
         self.longitude = longitude
         self.points_of_interest = points_of_interest
 
     def get_index(self) -> int:
+        """Gets the index of the Node.
+
+        Returns:
+            int: the index value
+        """
         return self.index
 
     def get_latitude(self) -> float:
+        """Gets the latitude of the street intersection.
+
+        Returns:
+            float: the latitude value
+        """
         return self.latitude
 
     def get_longitude(self) -> float:
+        """Gets the longitude of the street intersection.
+
+        Returns:
+            float: the longitude value
+        """
         return self.longitude
 
     def get_num_points_of_interest(self) -> int:
+        """Gets the number of the points of interest visible from the street intersection.
+
+        Returns:
+            int: number of the points of interest
+        """
         return len(self.points_of_interest)
 
     def get_points_of_interest(self) -> [str]:
+        """Gets the names of the points of interest visible from the street intersection.
+
+        Returns:
+            [str]: names of the points of interest
+        """
         return self.points_of_interest
